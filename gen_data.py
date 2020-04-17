0f3e3562ce83dc4f06c48401d9bd453347396f9f
Mario Molitor <mario.molitor@hbm.com>
2020-04-17 17:59:18 +0200

    adding plot for gen_data

--- gen_data.py
+++ gen_data.py
@@ -10,6 +10,8 @@ Created on Fri Apr 17 10:20:43 2020
 
 from numpy import cos, sin, pi, arange
 import numpy as np
+import pylab as pl
+
 
 #------------------------------------------------
 # Create a signal for demonstration.
@@ -19,8 +21,8 @@ sample_rate = 1000.0
 nsamples = 3000
 scaling_factor = 2**12
 t = arange(nsamples) / sample_rate
-#x = cos(2*pi*50*t) + 0.2*sin(2*pi*2.5*t+0.1) + 0.2*sin(2*pi*15.3*t) + 0.1*sin(2*pi*16.7*t + 0.1) + 0.1*sin(2*pi*23.45*t+.8)
-x = cos(2*pi*50*t) + 0.2*sin(2*pi*60*t+0.1)
+x = cos(2*pi*50*t) + 0.2*sin(2*pi*2.5*t+0.1) + 0.2*sin(2*pi*15.3*t) + 0.1*sin(2*pi*16.7*t + 0.1) + 0.1*sin(2*pi*23.45*t+.8)
+#x = cos(2*pi*50*t)  + 0.2*sin(2*pi*60*t+0.1)
 x = np.round(x * scaling_factor);
 file=open("unfiltered.dat","w")
 s = 1;
@@ -29,3 +31,11 @@ for d in x:
    file.write(str(s) + " " + str(i) + " " + str(i) + " " + str(i)  + "\n")
    s = s + 1
 file.close()
+
+pl.title('Fixed point filtering demo');
+# unfiltered
+pl.subplot(211);
+pl.plot(x);
+pl.xlabel('samples');
+pl.ylabel('unfiltered/raw ADC units');
+pl.show()
